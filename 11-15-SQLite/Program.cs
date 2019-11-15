203641e637ddb9c75957e72a96594cab04efb531
Metaa <asd@asd.asd>
2019-11-15 10:20:13 +0100

    Add project files.

---
+++ 11-15-SQLite/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_15_SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       //ConnectionStrings.com
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=mydb.db"))
+            { 
+                conn.Open();
+            }
+        }
+    }
+}
